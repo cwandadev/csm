63afe5a41167b020a2912bcf8ee88b1d022fd449
Cw <144074491+cwandadev@users.noreply.github.com>
2026-04-28 03:04:59 +0200

    Delete csms-frontend directory

--- csms-frontend/src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import 'boxicons/css/boxicons.min.css';
-
-createRoot(document.getElementById("root")!).render(<App />);
